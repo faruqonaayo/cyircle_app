297ae1faf403dde3e6db7263d0fa6b442485d9e6
Faruq Ayomide <onaayofaruq24@gmail.com>
2025-05-31 15:48:50 -0600

    Implement MyCategoryProvider for managing user categories with Riverpod

---
+++ lib/providers/my_category_provider.dart
@@ -0,0 +1,26 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import 'package:cyircle_app/models/category.dart';
+import 'package:cyircle_app/services/category_service.dart';
+
+class MyCategoryProviderNotifier extends StateNotifier<List<Category>> {
+  MyCategoryProviderNotifier() : super([]);
+
+  void loadCategories() async {
+    final categories = await CategoryService().getUserCategories(
+      FirebaseAuth.instance.currentUser!.uid,
+    );
+
+    state = categories;
+  }
+
+  void addCategory(Category category) {
+    state = [...state, category];
+  }
+}
+
+final myCategoryProvider =
+    StateNotifierProvider<MyCategoryProviderNotifier, List<Category>>((ref) {
+      return MyCategoryProviderNotifier();
+    });
--- lib/services/category_service.dart
+++ lib/services/category_service.dart
@@ -1,21 +1,43 @@
-import 'dart:convert';
-
 import 'package:cloud_firestore/cloud_firestore.dart';
+
 import 'package:cyircle_app/models/category.dart';
 
 class CategoryService {
-  const CategoryService();
+  CategoryService();
+
+  final fireStore = FirebaseFirestore.instance;
 
   Future<DocumentReference?> saveCategory(Category category) async {
-    final newCategoryRef = await FirebaseFirestore.instance
-        .collection("categories")
-        .add({
-          "name": category.name,
-          "description": category.description,
-          "colors": category.colors,
-          "userId": category.userId,
-        });
+    final newCategoryRef = await fireStore.collection("categories").add({
+      "name": category.name,
+      "description": category.description,
+      "colors": category.colors,
+      "userId": category.userId,
+    });
 
     return newCategoryRef;
+  }
+
+  Future<List<Category>> getUserCategories(String userId) async {
+    final userCategories = await fireStore
+        .collection("categories")
+        .where("userId", isEqualTo: userId)
+        .get();
+
+    final List<Category> userCategoriesList = [];
+
+    for (var doc in userCategories.docs) {
+      final data = doc.data();
+      userCategoriesList.add(
+        Category(
+          name: data["name"],
+          description: data["description"],
+          colors: data["colors"],
+          userId: userId,
+        ),
+      );
+    }
+
+    return userCategoriesList;
   }
 }
